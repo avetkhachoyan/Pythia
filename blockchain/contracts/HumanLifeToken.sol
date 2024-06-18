64d137cd8cb589dca90b81c3808c871800632206
avetkhachoyan <avetkhachoyan@gmail.com>
2024-06-18 06:58:44 -0400

    HumanLifeToken

---
+++ blockchain/contracts/HumanLifeToken.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HumanLifeToken is ERC721Enumerable, Ownable {
+    struct LifeEvent {
+        string eventType;
+        uint256 timestamp;
+        string details;
+    }
+
+    struct HumanLife {
+        string name;
+        string birthDate;
+        string birthPlace;
+        string gender;
+        LifeEvent[] events;
+    }
+
+    mapping(uint256 => HumanLife) public humanLives;
+    mapping(address => bool) public hasConnected;
+
+    event FirstTimeConnected(address indexed user);
+
+    constructor() ERC721("HumanLifeToken", "HLT") {}
+
+    function mintFirstTimeToken(address to) public {
+        require(!hasConnected[to], "Token already minted for this address");
+        uint256 tokenId = totalSupply() + 1;
+        _mint(to, tokenId);
+        hasConnected[to] = true;
+        emit FirstTimeConnected(to);
+    }
+
+    function addLifeEvent(uint256 tokenId, string memory eventType, uint256 timestamp, string memory details) public {
+        require(ownerOf(tokenId) == msg.sender, "You do not own this token");
+        humanLives[tokenId].events.push(LifeEvent(eventType, timestamp, details));
+    }
+
+    function setHumanLife(uint256 tokenId, string memory name, string memory birthDate, string memory birthPlace, string memory gender) public {
+        require(ownerOf(tokenId) == msg.sender, "You do not own this token");
+        humanLives[tokenId] = HumanLife(name, birthDate, birthPlace, gender, new LifeEvent[](0));
+    }
+
+    function getHumanLife(uint256 tokenId) public view returns (HumanLife memory) {
+        return humanLives[tokenId];
+    }
+
+    function getLifeEvents(uint256 tokenId) public view returns (LifeEvent[] memory) {
+        return humanLives[tokenId].events;
+    }
+}
--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,21 +1,33 @@
+let contract;
+let account;
+
 async function connectMetaMask() {
     if (window.ethereum) {
         try {
             const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            const account = accounts[0];
+            account = accounts[0];
             document.getElementById('account').innerText = `Account: ${account}`;
-
-            const balance = await ethereum.request({
-                method: 'eth_getBalance',
-                params: [account, 'latest']
-            });
-            const etherBalance = parseFloat(ethers.utils.formatEther(balance)).toFixed(4);
-            document.getElementById('balance').innerText = `Balance: ${etherBalance} ETH`;
+            await loadContract();
         } catch (error) {
             console.error('User denied account access', error);
         }
     } else {
         console.log('MetaMask is not installed');
+    }
+}
+
+async function loadContract() {
+    const contractAddress = 'HumanLifeToken_ADDRESS_HERE';
+    const abi = [/* ABI goes here */];
+    const web3 = new Web3(window.ethereum);
+    contract = new web3.eth.Contract(abi, contractAddress);
+
+    const hasConnected = await contract.methods.hasConnected(account).call();
+    if (!hasConnected) {
+        await contract.methods.mintFirstTimeToken(account).send({ from: account });
+        alert('First time connected to Web3 Life token minted!');
+    } else {
+        alert('Welcome back! You already have a Web3 Life token.');
     }
 }
 
